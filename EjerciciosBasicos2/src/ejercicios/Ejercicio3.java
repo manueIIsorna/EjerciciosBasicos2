64ccaf4d4d3b11de8262e3bdac2578dc72bcb4c8
Is örn <manuel.isorna@iesnervion.es>
2022-09-27 12:41:51 +0200

    Commit 1

---
+++ EjerciciosBasicos2/src/ejercicios/Ejercicio3.java
@@ -0,0 +1,27 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ * Escribir un programa que pida un número al usuario e indique mediante un
+ * literal booleano (true o false) si el número es par.
+ */
+public class Ejercicio3 {
+	public static void main(String[] args) {
+		// bloque de variables
+		Scanner sc = new Scanner(System.in);
+		int num;
+
+		// bloque de instrucciones
+		System.out.print("Introduzca un numero entero: ");
+		num = sc.nextInt();
+		// Imprimimos el resultado
+		if (num % 2 == 0) {
+			System.out.println("Es un número par");
+		} else {
+			System.out.println("Es un número impar");
+		}
+		// cerramos el Scanner
+		sc.close();
+	}
+}
